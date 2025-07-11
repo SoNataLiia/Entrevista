b2b273f989b25224fb08797272fff46ed5e98098
sokha <sokhatska.n@gmail.com>
2025-07-11 16:42:52 +0200

    Primer commit

---
+++ src/ReverseString.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce una palabra para hacer revers: ");
+        String palabra = sc.nextLine();
+        String reversed = new StringBuilder(palabra).reverse().toString();
+        System.out.println(reversed);
+    }
+}
